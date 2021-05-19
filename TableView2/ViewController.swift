60ba11b0bfc715d5c5c09d159bb579dceeb3da0e
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-18 22:49:03 -0600

    Initial Commit

---
+++ TableView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView2
+//
+//  Created by Kien Nguyen on 2021-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
